51ce36124a42388d031ca2486b65e84e79291f33
Dawid Janiga <dawidjaniga@gmail.com>
2019-03-18 15:12:26 +0100

    feat(components): Add Header

---
+++ resources/assets/app/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Layout, Menu } from 'antd'
+
+const { Header } = Layout
+
+export default function () {
+  return (
+    <Header>
+      <div className='logo' />
+      <Menu
+        theme='dark'
+        mode='horizontal'
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key='1'>Login</Menu.Item>
+        <Menu.Item key='2'>Practice</Menu.Item>
+        <Menu.Item key='3'>Progress</Menu.Item>
+      </Menu>
+    </Header>
+  )
+}
