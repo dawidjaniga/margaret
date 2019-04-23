e4aa243a5eecba15406f2cc6e948431b3043d768
Dawid Janiga <dawidjaniga@gmail.com>
2019-04-23 11:57:58 +0200

    feat(login): Redirect to FRONTEND_URL env on success

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const path = require('path')
 const publicPath = path.join(__dirname, 'public')
 
@@ -26,5 +27,8 @@ module.exports = {
         loader: ['babel-loader', 'react-hot-loader/webpack']
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['APP_URL', 'FRONTEND_URL'])
+  ]
 }
